e712a7ee7e25b71ba08c86a95075ab9d569a2b43
Venkatesh Thommandru <tvenky359@gmail.com>
2026-01-28 09:01:31 +0530

    Updated

--- basics/Normal-scripts/1-installation.sh
+++ basics/Normal-scripts/1-installation.sh
@@ -7,11 +7,12 @@ if [ $USERID -ne 0 ];then
    exit 1
 fi 
 
-dnf install mysql -y
+echo "Installing Nginx"
+dnf install Nginx -y
 
 if [$? -ne 0 ];then
-    echo "Error :: Installing Mysql is Failure"
+    echo "Error :: Installing Nginx is Failure"
     exit 1
 else
-    echo "Installing MySQL is SUCCESS"
+    echo "Installing Nginx is SUCCESS"
 fi          
