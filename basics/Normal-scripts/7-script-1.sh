5d72df9b6245ceb8708c368afec4ce891279e53d
Venkatesh Thommandru <venkatesh.thomm@gmail.com>
2026-03-28 13:18:50 +0530

    comments added

--- basics/Normal-scripts/7-script-1.sh
+++ basics/Normal-scripts/7-script-1.sh
@@ -8,9 +8,9 @@ echo "My Country NAME :" $NAME
 echo "PID SCRIPT-1:" $$
 
 # this mode will give different pid for both files
-# Direct calling of file 
+# Direct calling of file  call by sh <script-name> runs in different process, can't access script-1 environment. script-1 is not effected by script-2 failure
 #sh 8-script-2.sh
 
 # this mode will give same pid for both files
-# Indirect calling of file with source command [ . represent current directory ]
+# Indirect calling of file with source command [ . represent current directory ] source <script-name> runs in the same proces sof script-1 can access script-1 environment. script-1 is effected by script-2 failure
 source ./8-script-2.sh
